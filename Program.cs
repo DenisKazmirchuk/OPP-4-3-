196122454a0d2c2b0965a9f073dcb442a38c5ccd
User <User@DESKTOP-I3Q0DFC>
2024-10-08 21:08:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿partial class Department
+{
+    private string departmentName;
+    private int teacherCount;
+
+    public void SetDepartmentName(string name)
+    {
+        departmentName = name;
+    }
+
+    public void SetTeacherCount(int count)
+    {
+        teacherCount = count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Кафедра: {departmentName}, кількість викладачів: {teacherCount}");
+    }
+}
